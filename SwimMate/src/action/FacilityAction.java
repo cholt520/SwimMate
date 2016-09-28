2279eeafddeb4d9a291eb604a0369ab729caa874
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 14:43:03 +1000

    Facilityaction comments

--- SwimMate/src/action/FacilityAction.java
+++ SwimMate/src/action/FacilityAction.java
@@ -12,14 +12,32 @@ public class FacilityAction{
 		private String selectedFacility = "";
 		private String facilityName = "";
 		
+		/**
+		 * getSelectedFacility 
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public String getSelectedFacility() {
 			return selectedFacility;
 		}
 
+		/**
+		 * setSelectedFacility 
+		 *
+		 * @param selectedFacility
+		 * @throws java.Nullpoint.exception
+		 */
 		public void setSelectedFacility(String selectedFacility) {
 			this.selectedFacility = selectedFacility;
 		}
 		
+		/**
+		 *  get All Facility to list and display it on facility.jsp page
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public String execute(){
 			try {
 				facilitiesList = facilityService.getAllFacility();
@@ -29,7 +47,12 @@ public class FacilityAction{
 			return "success";
 		}
 		
-		//FindFacility initial page
+		/**
+		 * get All Facility to list and display it on facility.jsp page 
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public String getFacility(){
 			try {
 				facilitiesList = facilityService.getAllFacility();
@@ -39,7 +62,12 @@ public class FacilityAction{
 			return "success";
 		}
 
-		//Find beach by facility
+		/**
+		 * Find Beach By Facility 
+		 *
+		 * @param selectedFacility
+		 * @throws java.Nullpoint.exception
+		 */
 		public String findBeachByFacility() {
 			try {
 				facilitiesList = facilityService.getBeachByFacility(selectedFacility);
@@ -49,26 +77,62 @@ public class FacilityAction{
 			return "success";
 		}
 		
+		/**
+		 * getFacilityService 
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public FacilityService getFacilityService() {
 			return facilityService;
 		}
 
+		/**
+		 * setFacilityService 
+		 *
+		 * @param facilityService
+		 * @throws java.Nullpoint.exception
+		 */
 		public void setFacilityService(FacilityService facilityService) {
 			this.facilityService = facilityService;
 		}
 
+		/**
+		 * getFacilitiesList 
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public List<Facility> getFacilitiesList() {
 			return facilitiesList;
 		}
 
+		/**
+		 * set Facilities List 
+		 *
+		 * @param facilitiesList
+		 * @throws java.Nullpoint.exception
+		 */
 		public void setFacilitiesList(List<Facility> facilitiesList) {
 			this.facilitiesList = facilitiesList;
 		}
 
+		/**
+		 * getFacilityName 
+		 *
+		 * @param none
+		 * @throws java.Nullpoint.exception
+		 */
 		public String getFacilityName() {
 			return facilityName;
 		}
 
+		/**
+		 * setFacilityName 
+		 *
+		 * @param facilityName
+		 * @throws java.Nullpoint.exception
+		 */
 		public void setFacilityName(String facilityName) {
 			this.facilityName = facilityName;
 		}
