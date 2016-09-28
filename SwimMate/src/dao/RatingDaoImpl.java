be69693094f58fb377e13a1225ac4ca0ec1ff5f4
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 16:23:17 +1000

    comments

--- SwimMate/src/dao/RatingDaoImpl.java
+++ SwimMate/src/dao/RatingDaoImpl.java
@@ -8,11 +8,22 @@ import entity.Rating;
 
 public class RatingDaoImpl extends HibernateDaoSupport implements RatingDao{
 
+	/**
+	* Get all rating from database
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<Rating> getAllRating() {
 		return (List<Rating>)getHibernateTemplate().find("from Rating order by id desc");
 	}
 
-	
+	/**
+	* add rating from database
+	*
+	* @param rating 
+	* @throws java.Nullpoint.exception
+	*/
 	public void addRating(Rating rating) {
 		System.out.println(rating);
 		getHibernateTemplate().getSessionFactory().openSession().save(rating);
