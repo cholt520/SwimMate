4d3dee5f5e5330cab5a53ed70d6a86bb45498d6d
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 18:22:54 +1000

    comments

--- SwimMate/src/entity/SharkAlarm.java
+++ SwimMate/src/entity/SharkAlarm.java
@@ -8,45 +8,115 @@ public class SharkAlarm {
 	private String location;
 	private String latitude;
 	private String longtitude;
+	
+	/**
+	* This is a getter which gets the id
+	@param id - the id to be get
+	*/
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	* This is a setter which sets the id
+	@param id - the id to be set
+	*/
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	* This is a getter which gets the speciesname
+	@param speciesname - the speciesname to be get
+	*/
 	public String getSpeciesname() {
 		return speciesname;
 	}
+	
+	/**
+	* This is a setter which sets the speciesname
+	@param speciesname - the speciesname to be set
+	*/
 	public void setSpeciesname(String speciesname) {
 		this.speciesname = speciesname;
 	}
+	
+	/**
+	* This is a getter which gets the date
+	@param date - the date to be get
+	*/
 	public String getDate() {
 		return date;
 	}
+	
+	/**
+	* This is a setter which sets the date
+	@param date - the date to be set
+	*/
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	/**
+	* This is a getter which gets the area
+	@param area - the area to be get
+	*/
 	public String getArea() {
 		return area;
 	}
+	
+	/**
+	* This is a setter which sets the area
+	@param area - the area to be set
+	*/
 	public void setArea(String area) {
 		this.area = area;
 	}
+	
+	/**
+	* This is a getter which gets the location
+	@param location - the location to be get
+	*/
 	public String getLocation() {
 		return location;
 	}
+	
+	/**
+	* This is a setter which sets the location
+	@param location - the location to be set
+	*/
 	public void setLocation(String location) {
 		this.location = location;
 	}
+	
+	/**
+	* This is a getter which gets the latitude
+	@param latitude - the latitude to be get
+	*/
 	public String getLatitude() {
 		return latitude;
 	}
+	
+	/**
+	* This is a setter which sets the latitude
+	@param latitude - the latitude to be set
+	*/
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+	
+	/**
+	* This is a getter which gets the longtitude
+	@param longtitude - the longtitude to be get
+	*/
 	public String getLongtitude() {
 		return longtitude;
 	}
+	
+	/**
+	* This is a setter which sets the longtitude
+	@param longtitude - the longtitude to be set
+	*/
 	public void setLongtitude(String longtitude) {
 		this.longtitude = longtitude;
 	}
