2e8905ab1a7a8b9f6c3a7a77aceec24ea717b51c
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 17:36:45 +1000

    userService comments

--- SwimMate/src/service/UserService.java
+++ SwimMate/src/service/UserService.java
@@ -5,10 +5,52 @@ import java.util.List;
 import entity.User;
 
 public interface UserService {
+	
+	/**
+	* Get User By Id
+	*
+	* @param id 
+	* @throws java.Nullpoint.exception
+	*/
 	public User getUserById(int id);
+	
+	/**
+	* Get all Users
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<User> getAllUser();
+	
+	/**
+	* Delete User
+	*
+	* @param user 
+	* @throws java.Nullpoint.exception
+	*/
 	public void deleteUser(User user);
+	
+	/**
+	* Add User
+	*
+	* @param user 
+	* @throws java.Nullpoint.exception
+	*/
 	public void addUser(User user);
+	
+	/**
+	* Change User
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public void modifyUser(User user);
+	
+	/**
+	* Get User By UserName
+	*
+	* @param username 
+	* @throws java.Nullpoint.exception
+	*/
 	public User getUserByUserName(String username);
 }
