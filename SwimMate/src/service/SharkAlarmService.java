00dedba6f7aa87f6a2db7a13a5c1ef7ef09ce105
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 17:24:46 +1000

    sharkalaerm comments

--- SwimMate/src/service/SharkAlarmService.java
+++ SwimMate/src/service/SharkAlarmService.java
@@ -5,5 +5,12 @@ import java.util.List;
 import entity.SharkAlarm;
 
 public interface SharkAlarmService {
+	
+	/**
+	* Get All Shark Alarm 
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<SharkAlarm> getAllSharkAlarm();
 }
