00e01bd653479b59515221a4c285c0bc5a75833d
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 18:37:56 +1000

    wind comments

--- SwimMate/src/entity/Wind.java
+++ SwimMate/src/entity/Wind.java
@@ -5,21 +5,50 @@ public class Wind {
 	private double deg;
 	private double gust;
 	
+	/**
+	* This is a getter which gets the speed
+	@param speed - the speed to be get
+	*/
 	public double getSpeed() {
 		return speed;
 	}
+	
+	/**
+	* This is a setter which sets the speed
+	@param speed - the speed to be set
+	*/
 	public void setSpeed(double speed) {
 		this.speed = speed;
 	}
+	
+	/**
+	* This is a getter which gets the deg
+	@param deg - the deg to be get
+	*/
 	public double getDeg() {
 		return deg;
 	}
+	
+	/**
+	* This is a setter which sets the deg
+	@param deg - the deg to be set
+	*/
 	public void setDeg(double deg) {
 		this.deg = deg;
 	}
+	
+	/**
+	* This is a getter which gets the gust
+	@param gust - the gust to be get
+	*/
 	public double getGust() {
 		return gust;
 	}
+	
+	/**
+	* This is a setter which sets the gust
+	@param gust - the gust to be set
+	*/
 	public void setGust(double gust) {
 		this.gust = gust;
 	}
