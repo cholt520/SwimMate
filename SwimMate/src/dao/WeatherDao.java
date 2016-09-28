876c5e3984f3dc7b42a7efcbed8db600e08c796e
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 16:46:41 +1000

    COMMENTS

--- SwimMate/src/dao/WeatherDao.java
+++ SwimMate/src/dao/WeatherDao.java
@@ -2,6 +2,12 @@ package dao;
 
 import entity.Weather;
 
+/**
+* The weather API instead of this interface
+*
+* @param none 
+* @throws java.Nullpoint.exception
+*/
 public interface WeatherDao {
 	public Weather getWeatherById();
 	public void addWeather(Weather weather);
--- SwimMate/src/dao/WeatherDaoImpl.java
+++ SwimMate/src/dao/WeatherDaoImpl.java
@@ -4,29 +4,31 @@ import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
 import entity.Weather;
 
+/**
+* The weather API instead of this interface
+*
+* @param none 
+* @throws java.Nullpoint.exception
+*/
 public class WeatherDaoImpl extends HibernateDaoSupport implements WeatherDao{
 
 	@Override
 	public Weather getWeatherById() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void addWeather(Weather weather) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Weather modifyWeather(Weather weather) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void deleteWeatherById(int id) {
-		// TODO Auto-generated method stub
 		
 	}
 
