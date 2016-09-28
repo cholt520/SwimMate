bf2dc20cb50690b987e7010491904e7753878208
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 16:59:42 +1000

    comments

--- SwimMate/src/service/PatrolService.java
+++ SwimMate/src/service/PatrolService.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import entity.Patrol;
 
+/**
+* This interface is not implementing now,the change will be on future
+*
+* @param none 
+* @throws java.Nullpoint.exception
+*/
 public interface PatrolService {
 	public Patrol getPatrolDaoById(int id);
 	public List<Patrol> getAllPatrol();
 	public void deletePatrol(Patrol patrol);
 	public void addPatrol(Patrol patrol);
 	public void modifyPatrol(Patrol patrol);
-	//dkfjghfjhgdjkfgdfkh
 }
