bfdf006e425cdbd4b99f979f3fb5451091cea543
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 15:45:12 +1000

    Tip action comments

--- SwimMate/src/action/TipAction.java
+++ SwimMate/src/action/TipAction.java
@@ -10,26 +10,72 @@ public class TipAction implements Action{
 	private User currentLoginUser;
 	private UserService userService;
 	
+	/**
+	* getLoginUserID 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public int getLoginUserID() {
 		return loginUserID;
 	}
+	
+	/**
+	* setLoginUserID
+	*
+	* @param loginUserID
+	* @throws java.Nullpoint.exception
+	*/
 	public void setLoginUserID(int loginUserID) {
 		this.loginUserID = loginUserID;
 	}
+	
+	/**
+	* getCurrentLoginUser 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public User getCurrentLoginUser() {
 		return currentLoginUser;
 	}
+	
+	/**
+	* setCurrentLoginUser
+	*
+	* @param currentLoginUser
+	* @throws java.Nullpoint.exception
+	*/
 	public void setCurrentLoginUser(User currentLoginUser) {
 		this.currentLoginUser = currentLoginUser;
 	}
+	
+	/**
+	* getUserService
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public UserService getUserService() {
 		return userService;
 	}
+	
+	/**
+	* setUserService 
+	*
+	* @param userService
+	* @throws java.Nullpoint.exception
+	*/
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
 	
-	@Override
+	/**
+	* show current login user
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public String execute() throws Exception {
 		try {
 			if (loginUserID != -1) {
