fc5bf6be1dba872d522b9db5fd010a08fec4dd97
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 16:37:20 +1000

    comments

--- SwimMate/src/dao/TrainingDao.java
+++ SwimMate/src/dao/TrainingDao.java
@@ -5,12 +5,61 @@ import java.util.List;
 import entity.Training;
 
 public interface TrainingDao {
+	
+	/**
+	* Get Training By Id
+	*
+	* @param id 
+	* @throws java.Nullpoint.exception
+	*/
 	public Training getTrainingById(int id);
+	
+	/**
+	* Get all Training
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<Training> getAllTraining();
+	
+	/**
+	* Delete all Training
+	*
+	* @param none 
+	* @throws java.Nullpoint.exception
+	*/
 	public void deleteTraining(Training training);
+	
+	/**
+	* Add all Training
+	*
+	* @param training 
+	* @throws java.Nullpoint.exception
+	*/
 	public void addTraining(Training training);
+	
+	/**
+	* Get all Training
+	*
+	* @param training 
+	* @throws java.Nullpoint.exception
+	*/
 	public void modifyTraining(Training training);
+	
+	/**
+	* Get Training by state
+	*
+	* @param state 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<Training> getTrainingByState(String state);
+	
+	/**
+	* Get Training By Postcode
+	*
+	* @param postcode 
+	* @throws java.Nullpoint.exception
+	*/
 	public List<Training> getTrainingByPostcode(String postcode);
 
 }
