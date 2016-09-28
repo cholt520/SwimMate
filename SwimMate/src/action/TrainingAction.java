4999727c93f52f52b6ccb3705153204b0dfd8958
junwang <junwang@dyn-118-139-8-112.its.monash.edu.au>
2016-09-28 15:54:00 +1000

    Trainingaction comments

--- SwimMate/src/action/TrainingAction.java
+++ SwimMate/src/action/TrainingAction.java
@@ -20,31 +20,72 @@ public class TrainingAction implements Action{
 	private User currentLoginUser;
 	private UserService userService;
 	
-	
+	/**
+	* getLoginUserID 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public int getLoginUserID() {
 		return loginUserID;
 	}
 
+	/**
+	* setLoginUserID 
+	*
+	* @param loginUserID
+	* @throws java.Nullpoint.exception
+	*/
 	public void setLoginUserID(int loginUserID) {
 		this.loginUserID = loginUserID;
 	}
 
+	/**
+	* getCurrentLoginUser 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public User getCurrentLoginUser() {
 		return currentLoginUser;
 	}
 
+	/**
+	* setCurrentLoginUser 
+	*
+	* @param currentLoginUser
+	* @throws java.Nullpoint.exception
+	*/
 	public void setCurrentLoginUser(User currentLoginUser) {
 		this.currentLoginUser = currentLoginUser;
 	}
 
+	/**
+	* getUserService 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public UserService getUserService() {
 		return userService;
 	}
 
+	/**
+	* setUserService 
+	*
+	* @param userService
+	* @throws java.Nullpoint.exception
+	*/
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
 
+	/**
+	* Get user information and relating to training page. 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public String execute(){
 		try {
 			if (loginUserID != -1) {
@@ -57,6 +98,12 @@ public class TrainingAction implements Action{
 		return "success";
 	}
 	
+	/**
+	* Get all Training By State 
+	*
+	* @param loginUserID,state
+	* @throws java.Nullpoint.exception
+	*/
 	public String getTrainingByState(){
 		try {
 			if (loginUserID != -1) {
@@ -69,6 +116,12 @@ public class TrainingAction implements Action{
 		return "success";
 	}
 	
+	/**
+	* Get Training list By Postcode 
+	*
+	* @param loginUserID,postcode
+	* @throws java.Nullpoint.exception
+	*/
 	public String getTrainingByPostcode(){
 		try {
 			if (loginUserID != -1) {
@@ -81,34 +134,82 @@ public class TrainingAction implements Action{
 		return "success";
 	}
 	
+	/**
+	* getTrainingService 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public TrainingService getTrainingService() {
 		return trainingService;
 	}
 
+	/**
+	* setTrainingService 
+	*
+	* @param trainingService
+	* @throws java.Nullpoint.exception
+	*/
 	public void setTrainingService(TrainingService trainingService) {
 		this.trainingService = trainingService;
 	}
 
+	/**
+	* getTraininglist 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public List<Training> getTraininglist() {
 		return traininglist;
 	}
 
+	/**
+	* setTraininglist 
+	*
+	* @param traininglist
+	* @throws java.Nullpoint.exception
+	*/
 	public void setTraininglist(List<Training> traininglist) {
 		this.traininglist = traininglist;
 	}
 
+	/**
+	* getState 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public String getState() {
 		return state;
 	}
 
+	/**
+	* setState 
+	*
+	* @param state
+	* @throws java.Nullpoint.exception
+	*/
 	public void setState(String state) {
 		this.state = state;
 	}
 
+	/**
+	* getPostcode 
+	*
+	* @param none
+	* @throws java.Nullpoint.exception
+	*/
 	public String getPostcode() {
 		return postcode;
 	}
 
+	/**
+	* setPostcode 
+	*
+	* @param postcode
+	* @throws java.Nullpoint.exception
+	*/
 	public void setPostcode(String postcode) {
 		this.postcode = postcode;
 	}
